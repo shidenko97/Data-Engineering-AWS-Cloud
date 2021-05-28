bdc5832e7acf9a5240ea25b4f73e9360af37455f
Sergey Hidenko <shidenko97@gmail.com>
2021-05-28 14:06:34 +0300

    AWS Lambda order items import script

--- lambda-triggers/trigger_orders_items_hourly.py
+++ lambda-triggers/trigger_orders_items_hourly.py
@@ -1,15 +1,18 @@
 import json
 import boto3 
 
+
 def lambda_handler(event, context):
+    job_name = "hourly_order_items_import"
+
     client = boto3.client("glue")
 
     client.start_job_run(
-        JobName = 'glue_import_order_items_hourly',
-        Arguments = {}
+        JobName=job_name,
+        Arguments={}
     )
+
     return {
-        'statusCode': 200,
-        'body': json.dumps('glue_import_order_items_hourly triggered')
+        "statusCode": 200,
+        "body": json.dumps(f"{job_name} triggered")
     }
-    
